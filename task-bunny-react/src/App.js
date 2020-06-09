5f2fa56a5a7f2605fbeffb8b6b03adaea72d2479
Chinchinberry <jonjon0424@gmail.com>
2020-06-08 17:07:15 -0700

    added them deps

--- task-bunny-react/src/App.js
+++ task-bunny-react/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import LoginPage from './components/login/LoginPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <BrowserRouter>
+        <ToastContainer autoClose={3000} hideProgressBar />
+        <Switch>
+          <Route path="/" component={LoginPage} exact />
+          {/* <Route component={NotFoundPage} /> */}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
