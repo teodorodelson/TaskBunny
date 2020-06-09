1c53158c88a9ba38adbce864292e14b2691cfdb5
Chinchinberry <jonjon0424@gmail.com>
2020-06-08 22:40:50 -0700

    login page

--- task-bunny-react/src/App.js
+++ task-bunny-react/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import LoginPage from './components/login/LoginPage'
+import HomePage from './components/home/HomePage'
+import RegisterPage from './components/register/RegisterPage'
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
         <ToastContainer autoClose={3000} hideProgressBar />
         <Switch>
           <Route path="/" component={LoginPage} exact />
+          <Route path="/home" component={HomePage} exact />
+          <Route path="/register" component={RegisterPage} exact />
+          <Route path="/" component={Clienttask} exact />
           {/* <Route component={NotFoundPage} /> */}
         </Switch>
       </BrowserRouter>
