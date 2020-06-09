a0b5191b89e7a6496cc44f138e637bac7a30db8a
Chinchinberry <jonjon0424@gmail.com>
2020-06-09 14:52:37 -0700

    minor fixes

--- task-bunny-react/src/App.js
+++ task-bunny-react/src/App.js
@@ -3,12 +3,20 @@ import { Switch, Route } from "react-router-dom";
 import ProviderPage from "./Pages/providerPage";
 import ClientPage from "./Pages/clientPage";
 import ProviderTasks from "./Pages/tasksOfProvider";
-import HomePage from "./Pages/homePage";
 import "./App.css";
+
+import LoginPage from './components/login/LoginPage'
+import HomePage from './components/home/HomePage'
+import RegisterPage from './components/register/RegisterPage'
+import Clienttask from "./components/ClientComponent/Clienttask";
 
 function App() {
   return (
     <Switch>
+      <Route path="/" component={LoginPage} exact />
+          <Route path="/home" component={HomePage}  />
+          <Route path="/register" component={RegisterPage}  />
+          <Route path="/" component={Clienttask}  />
       <Route path="/provider page">
         <ProviderPage />
       </Route>
@@ -17,9 +25,6 @@ function App() {
       </Route>
       <Route path="/provider tasks">
         <ProviderTasks />
-      </Route>
-      <Route path="/">
-        <HomePage />
       </Route>
     </Switch>
   );
