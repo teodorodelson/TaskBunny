a84ecda136f1c306fcae5867bad7a0ffb78e75dd
FavourAmayo <favourosagie123@gmail.com>
2020-06-16 01:05:42 -0700

    condiitionally render

--- task-bunny-react/src/components/common/Navbar.jsx
+++ task-bunny-react/src/components/common/Navbar.jsx
@@ -1,12 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function Navbar() {
   const activeStyle = { color: "#fffdd0", fontWeight: "bold" };
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    //console.log("Token", localStorage.getItem("token"));
+    if (localStorage.getItem("token") !== null) {
+      setLoggedIn(true);
+    }
+  }, isLoggedIn);
+
+  function handleClick(e) {
+    localStorage.removeItem("token");
+    toast.success("Logout Successful");
+    setLoggedIn(false);
+  }
+
+  function renderLogin() {
+    if (isLoggedIn) {
+      return (
+        <nav className="navbar navbar-dark bg-dark">
+          <NavLink
+            to="/home"
+            className="nav-link"
+            activeStyle={activeStyle}
+            exact
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/categories"
+            className="nav-link"
+            activeStyle={activeStyle}
+          >
+            Tasks
+          </NavLink>
+          <NavLink
+            className="nav-link"
+            to="/client page"
+            activeStyle={activeStyle}
+          >
+            Profile
+          </NavLink>
+          <NavLink
+            to="/"
+            className="nav-link"
+            activeStyle={activeStyle}
+            onClick={handleClick}
+            exact
+          >
+            Logout
+          </NavLink>
+        </nav>
+      );
+    } else {
+      return (
+        <nav className="navbar navbar-dark bg-dark">
+          <NavLink
+            to="/"
+            className="nav-link"
+            activeStyle={activeStyle}
+            onClick={handleClick}
+            exact
+          >
+            Login
+          </NavLink>
+        </nav>
+      );
+    }
+  }
+
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <NavLink
+      {/* <nav className="navbar navbar-dark bg-dark"> */}
+      {/* <NavLink
           to="/home"
           className="nav-link"
           activeStyle={activeStyle}
@@ -27,14 +98,15 @@ export default function Navbar() {
           activeStyle={activeStyle}
         >
           Profile
-        </NavLink>
-        <NavLink to="/" className="nav-link" activeStyle={activeStyle} exact>
+        </NavLink> */}
+      {renderLogin()}
+      {/* <NavLink to="/" className="nav-link" activeStyle={activeStyle} exact>
           Login
         </NavLink>
         <NavLink to="/" className="nav-link" activeStyle={activeStyle} exact>
           Logout
-        </NavLink>
-      </nav>
+        </NavLink> */}
+      {/* </nav> */}
     </div>
   );
 }
